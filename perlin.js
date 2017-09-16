c9f4598e8b04b26232ec21bbf90aef42cb5cac95
roadkillcat <joedominiciddon@gmail.com>
2017-09-16 18:47:45 +0100

    moves all code into a single object

--- perlin.js
+++ perlin.js
@@ -1,46 +1,50 @@
-function rand2dVect(){
-    var theta = Math.random() * 2 * Math.PI
-    return {x: Math.cos(theta), y: Math.sin(theta)}
-}
-
-function dotGridGradient(vertX, vertY, x, y){
-	gVect = gradient[vertY][vertX]
-	dVect = {x: x - vertX, y: y - vertY}
-	return dVect.x * gVect.x + dVect.y * gVect.y
-}
-
-var gradient
-var dotProduct = (vect1, vect2) => vect1.x * vect2.x + vect1.y + vect2.y
-var linTerp = (a, b, w) => (1 - w)*a + w*b
-
 var perlin = {
+	rand2dVect: function(){
+		var theta = Math.random() * 2 * Math.PI
+		return {x: Math.cos(theta), y: Math.sin(theta)}	
+	},
+	
+	dotGridGradient: function(vertX, vertY, x, y){
+		var gVect = this.gradients[vertY][vertX]
+		var dVect = {x: x - vertX, y: y - vertY}
+		return dVect.x * gVect.x + dVect.y * gVect.y
+	},
+	
+	linTerp: function(a, b, w){
+		return (1 - w) * a + w * b
+	},
+	
+	gradients: new Array,
+	
 	seed: function(){
-		gradient = []
 		for (var y = 0; y < 256; y++){
 			var row = []
 			for (var x = 0; x < 256; x++){
-				row.push(rand2dVect())
+				row.push(this.rand2dVect())
 			}
-			gradient.push(row)
+			this.gradients.push(row)
 		}
 	},
 
 	get: function(x, y) {
-		var cell = {x0: Math.floor(x), x1: Math.floor(x) + 1, y0: Math.floor(y), y1: Math.floor(y) + 1}
+		var x0 = Math.floor(x)
+		var x1 = x0 + 1
+		var y0 = Math.floor(y)
+		var y1 = y0 + 1
 		
-		var xWeight = x - cell.x0
-		var yWeight = y - cell.y0
+		var xWeight = x - x0
+		var yWeight = y - y0
 		
 		//interpolate
 		var tl, tr, bl, br, xTop, xBottom
 		
-		tl = dotGridGradient(cell.x0, cell.y0, x, y)
-		tr = dotGridGradient(cell.x1, cell.y0, x, y)
-		xTop = linTerp(tl, tr, xWeight)
-		bl = dotGridGradient(cell.x0, cell.y1, x, y)
-		br = dotGridGradient(cell.x1, cell.y1, x, y)
-		xBottom = linTerp(bl, br, xWeight)
-		return (linTerp(xTop, xBottom, yWeight) + 1) / 2
+		tl = this.dotGridGradient(x0, y0, x, y)
+		tr = this.dotGridGradient(x1, y0, x, y)
+		xTop = this.linTerp(tl, tr, xWeight)
+		bl = this.dotGridGradient(x0, y1, x, y)
+		br = this.dotGridGradient(x1, y1, x, y)
+		xBottom = this.linTerp(bl, br, xWeight)
+		return (this.linTerp(xTop, xBottom, yWeight) + 1) / 2
 	}
 }
 
